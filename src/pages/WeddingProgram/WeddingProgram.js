a1d3e53900efcb8c0e36b4469e5604b0788f3412
kenny oderinde <kennyoderyndeh@gmail.com>
2024-08-13 03:03:00 +0100

    changed leaves bg to black

--- src/pages/WeddingProgram/WeddingProgram.js
+++ src/pages/WeddingProgram/WeddingProgram.js
@@ -10,19 +10,25 @@ const PdfPage = () => {
     window.open(pdfUrl, '_blank');
   };
 
+  const openPdfReceptn = () => {
+    const pdfUrl = '/tork-reception-program-pdf-format.pdf'; // Assuming the file is in the public folder
+    window.open(pdfUrl, '_blank');
+  };
+
   return (
     <>
 
-     <div id="leaves">
+     <div id="leaves" >
       {[...Array(35)].map((_, index) => (
         <i key={index}></i>
       ))}
     </div>
 
-    <div className="container">
-      <div className='w-80 h-40 bg-white'>
-        <h1>PDF Page</h1>
-        <button onClick={openPdf}>
+    <div className="container bg-black -mt-20">
+      <div className='w-60 h-44 bg-white py-5 rounded-md'>
+        <h1 className=' text-md font-semibold'>Tork24 Church Program</h1>
+        <p className=' text-base font-medium'>Kindly click to view</p> 
+        <button onClick={openPdf} className='pdf-btn'>
           <span>Open PDF</span>
         </button>
       </div>
@@ -34,6 +40,17 @@ const PdfPage = () => {
       ))}
     </div>
 
+
+    <div className="container bg-black -mt-80">
+      <div className='w-60 h-44 bg-white py-5 rounded-md'>
+        <h1 className=' text-md font-semibold'>Tork24 Reception Program</h1>
+        <p className=' text-base font-medium'>Kindly click to view</p> 
+        <button onClick={openPdfReceptn} className='pdf-btn'>
+          <span>Open PDF</span>
+        </button>
+      </div>
+    </div>
+    
     </>
     
   );
